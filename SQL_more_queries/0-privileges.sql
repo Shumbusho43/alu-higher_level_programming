cf4ad1d4f420bdc2699bf7be1b753acac9ea43e6
Shumbusho43 <davidshumbusho10@gmail.com>
2025-03-13 07:30:35 -0700

    Simplify script to list privileges without conditional logic

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,5 @@
 -- Script that lists all privileges of MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Check if user_0d_2 exists before showing grants
-SELECT COUNT(*) INTO @user_exists FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost';
-
-IF @user_exists > 0 THEN
-    SHOW GRANTS FOR 'user_0d_2'@'localhost';
-ELSE
-    SELECT 'User user_0d_2 does not exist' AS Message;
-END IF;
+-- Attempt to show grants for user_0d_2, but suppress errors if the user does not exist
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
