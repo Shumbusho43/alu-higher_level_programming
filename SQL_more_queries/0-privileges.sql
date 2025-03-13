7d72f556cf01e6c2c2798b0c0bcc58b82ca9fc99
Shumbusho43 <davidshumbusho10@gmail.com>
2025-03-13 07:57:03 -0700

    Update script to correctly list privileges for user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,16 +1,5 @@
--- Create user_0d_1 if it does not exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user_0d_2 if it does not exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-GRANT SELECT, INSERT ON `user_2_db`.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
-
--- Show grants for user_0d_1
+-- Script that lists all privileges of MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Show grants for user_0d_2
+-- Attempt to show grants for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
